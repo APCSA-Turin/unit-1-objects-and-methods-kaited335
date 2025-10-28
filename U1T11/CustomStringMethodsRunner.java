510bf9c85bfe67640a421e35d53085827290af4e
kaited335 <katiec63@nycstudents.net>
2025-10-28 01:45:43 +0000

    .

--- U1T11/CustomStringMethodsRunner.java
+++ U1T11/CustomStringMethodsRunner.java
@@ -3,6 +3,13 @@ package U1T11;
 public class CustomStringMethodsRunner {
     public static void main(String[] args) {
         CustomStringMethods methods = new CustomStringMethods();
-        System.out.println(methods);
+        System.out.println(methods.longerThan("home", 7));
+        System.out.println(methods.funnyString("amazing", 3));
+        System.out.println(methods.halvesReversed("maize"));
+        System.out.println(methods.pigLatin("robin"));
+        System.out.println(methods.removeCharacter("monkey", 3));
+        System.out.println(methods.insertAt("water", "_bottle", "a"));
+        System.out.println(methods.endUp("people", 4));
+        System.out.println(methods.yellOrWhisper("Hello friend!"));
     }
 }
