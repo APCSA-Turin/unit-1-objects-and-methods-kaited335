76bf0a21857b82e2e74731138f68bbdcff1fcd95
kaited335 <katiec63@nycstudents.net>
2026-02-03 18:33:43 +0000

    .

--- U4T1/ArrayAlgorithms.java
+++ U4T1/ArrayAlgorithms.java
@@ -70,4 +70,82 @@ public class ArrayAlgorithms {
     }
     return combined;
   }  
+
+  public static void bookEnd(int[] nums, int num) {
+      for (int i = 0; i < nums.length; i++) {
+        if (i == 0) {
+            nums[i] = num;
+        }
+        if (i == nums.length - 1) {
+            nums[i] = num;
+        }
+      }
+  }
+
+  public static void multiplyBy(int[] numList, int multiplier) {
+    for (int i = 0; i < numList.length; i++) {
+        numList[i] = numList[i] * multiplier;
+    }
+  }
+
+  public static int[] multiplyByNoModify(int[] numList, int multiplier) {
+    int[] nums = new int[numList.length];
+    for (int i = 0; i < numList.length; i++) {
+        nums[i] = numList[i] * multiplier;
+    }
+    return nums;
+  }
+
+  public static void addExclamation(String[] wordList) {
+    for (int i = 0; i < wordList.length; i++) {
+        String end  = wordList[i].substring(wordList[i].length() - 1);
+        if ( end.equals("!")) {
+
+        } else {
+            wordList[i] = wordList[i] + "!";
+        }
+    }
+  }
+
+  public static boolean[] isFreezing(int[] tempList) {
+    boolean[] isTrue = new boolean[tempList.length];
+    for (int i = 0; i < tempList.length; i++) {
+        if (tempList[i] <= 32) {
+            isTrue[i] = true;
+        } else {
+            isTrue[i] = false;
+        }
+    }
+    return isTrue;
+  }
+
+  public static void shiftLeft(int[] numList) {
+     int temp = numList[0];
+     for (int i = 0; i < numList.length; i++) {
+        if (i == numList.length - 1) {
+            numList[i] = temp;
+        } else {
+            numList[i] = numList[i + 1];
+        }
+     }
+  }
+
+  public static void shiftRight(int[] numList) {
+    int temp = numList[numList.length - 1]; 
+    for (int i = numList.length - 1; i > -1; i--) {
+        if  (i == 0) {
+            numList[i] = temp;
+        } else {
+            numList[i] = numList[i - 1];
+        }
+    }
+  }
+
+  public static void reverse(int[] numList) { 
+    for (int i = 0; i < numList.length / 2; i++) {
+        int temp = numList[numList.length - 1 - i];
+        numList[numList.length - 1 - i] = numList[i];
+        numList[i] = temp;
+    }
+}
 }
--- U4T1/Main.java
+++ U4T1/Main.java
@@ -1,37 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums1 = {1, 2, 3, 4, 5, 6};
+        int[] nums = {6, 1, 3, 4, 7, 5};
 
-int[] nums2 = {10, 11, 12, 13, 14, 15, 16, 17};
+ArrayAlgorithms.shiftRight(nums);
 
-int[] combined = ArrayAlgorithms.combine(nums1, nums2);
+// original nums array IS modified; all elements shifted right 1
 
-for (int i = 0; i < combined.length; i++) {
+for (int i = 0; i < nums.length; i++) {
 
-    System.out.print(combined[i] + " ");
-
-}
-
-System.out.println();
-
-System.out.println("combined list has " + combined.length + " elements");
-
-
-
-int[] nums3 = {5, 8, 6, 2};
-
-int[] nums4 = {1, 7, 10, 0, 9};
-
-int[] combined2 = ArrayAlgorithms.combine(nums3, nums4);
-
-for (int i = 0; i < combined2.length; i++) {
-
-    System.out.print(combined2[i] + " ");
+    System.out.print(nums[i] + " ");
 
 }
 
 System.out.println();
 
-System.out.println("combined list has " + combined2.length + " elements");
+int[] nums2 = {1, 2, 3, 4, 5, 6, 7, 8};
+
+ArrayAlgorithms.shiftRight(nums2);
+
+// original nums2 array IS modified; all elements shifted right 1
+
+for (int i = 0; i < nums2.length; i++) {
+
+    System.out.print(nums2[i] + " ");
+
+}
+
+System.out.println();
+
+// shift nums2 AGAIN:
+
+ArrayAlgorithms.shiftRight(nums2);
+
+for (int i = 0; i < nums2.length; i++) {
+
+    System.out.print(nums2[i] + " ");
+
+}
+
+System.out.println();
+
+// shift nums2 A THIRD TIME:
+
+ArrayAlgorithms.shiftRight(nums2);
+
+for (int i = 0; i < nums2.length; i++) {
+
+    System.out.print(nums2[i] + " ");
+
+}
+
     }
 }
