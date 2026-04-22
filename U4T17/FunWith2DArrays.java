cbf550daba22f94b2deee80b62c3ba883a48215c
kaited335 <katiec63@nycstudents.net>
2026-04-22 04:17:42 +0000

    .

--- U4T17/FunWith2DArrays.java
+++ U4T17/FunWith2DArrays.java
@@ -48,6 +48,41 @@ public class FunWith2DArrays {
         return sum;
     }
 
-    
+    public static int[] indexFound(String[][] array, String target) {
+        int[] index = {-1, -1};
+        for (int i = 0; i < array[0].length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[j][i].equals(target)) {
+                    index = {i, j};
+                    return index;
+                }
+            }
+        }
+        return index;
+    }
+
+    public static int[][] split(int[][] array, int rowTarget, int columnTarget) {
+        if (rowTarget > array.length || columnTarget > array[0].length) {
+            return new int[0][0];
+        }
+        int[][] splitedArray = new int[rowTarget + 1][columnTarget + 1];
+        for (int row = 0; row < array.length; row++) {
+            for (int col = 0; col < array[0].length; col++) {
+                if (row <= rowTarget && col <= columnTarget) {
+                    splitedArray[row][col] = array[row][col]; 
+                }
+            }
+        } 
+        return splitedArray;
+    }
+    public static int[][] invert(int[][] array) {
+        int[][] inverted = new int[array[0].length][array.length];
+        for (int row = 0; row < array.length; row++) {
+            for (int col = 0; col < array[0].length; col++) {
+                inverted[col][row] = array[row][col];
+            }
+        }
+        return inverted;
+    }
 }
 
